368561a10eab7663843779f50c46986368ae8464
kimjihye058 <s2321@e-mirim.hs.kr>
2025-07-15 21:51:50 +0900

    [feat] : 사진 찍힌 후 저장 화면으로 넘어가기

--- src/picture/picture.jsx
+++ src/picture/picture.jsx
@@ -2,11 +2,12 @@ import './picture.css';
 import StepIndicator from '../stepIndicator/StepIndicator';
 import Webcam from 'react-webcam';
 import { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Picture = () => {
     const [countdown, setCountdown] = useState(5);
-    const [capturedImage, setCapturedImage] = useState(null);
     const webcamRef = useRef(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (countdown > 0) {
@@ -16,9 +17,12 @@ const Picture = () => {
             return () => clearTimeout(timer);
         } else if (countdown === 0 && webcamRef.current) {
             const imageSrc = webcamRef.current.getScreenshot();
-            setCapturedImage(imageSrc);
+
+            setTimeout(() => {
+                navigate('/selectPicture');
+            }, 500);
         }
-    }, [countdown]);
+    }, [countdown, navigate]);
 
     return (
         <div className='container'>
@@ -27,27 +31,20 @@ const Picture = () => {
                 <p className="headerKo">좌측의 카메라를 바라봐주세요!</p>
             </div>
             <div className="camera-area">
-                {!capturedImage ? (
-                    <>
-                        <Webcam
-                            audio={false}
-                            ref={webcamRef}
-                            screenshotFormat="image/jpeg"
-                            mirrored
-                            width={971}
-                            height={486}
-                            videoConstraints={{
-                                width: 971,
-                                height: 486
-                            }}
-                        />
-                        <div className="countdown">{countdown > 0 ? countdown : '찰칵!'}</div>
-                    </>
-                ) : (
-                    <div className="captured-image">
-                        <img src={capturedImage} alt="Captured" />
-                    </div>
-                )}
+                <Webcam
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    width={971}
+                    height={486}
+                    mirrored
+                    videoConstraints={{
+                        facingMode: 'user',
+                        width: 971,
+                        height: 486
+                    }}
+                />
+                <div className="countdown">{countdown}</div>
             </div>
             <StepIndicator currentStep={2} />
         </div>
