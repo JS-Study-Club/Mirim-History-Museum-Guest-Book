35f8af308006fe5d5928893f96c28a66a7762862
kimjihye058 <s2321@e-mirim.hs.kr>
2025-08-01 18:28:02 +0900

    [feat] : gallery의 사진이 좌우로 이동하는 애니메이션

--- src/gallery/gallery.jsx
+++ src/gallery/gallery.jsx
@@ -1,13 +1,43 @@
 import './gallery.css';
+import React, { useState } from 'react';
+
+  const repeatedImages = Array.from({ length: 10 });
 
 const Gallery = () => {
+
+    // const [pausedRow, setPausedRow] = useState(null);
+    // const [activeIndex, setActiveIndex] = useState(null);
+
+    // const handleClick = (rowIndex, index) => {
+    //     setPausedRow(rowIndex);
+    //     setActiveIndex(index);
+
+    //     // 3초 후 다시 재생
+    //     setTimeout(() => {
+    //     setPausedRow(null);
+    //     setActiveIndex(null);
+    //     }, 3000);
+    // };
 
     return(
         <div className='g-container'>
             <div className='g-header'>
-                <hr className='header-line'/>
                 <p className='header-text'>Mirim Gallery</p>
                 <hr className='header-line'/>
+            </div>
+            <div className="photo-scroller">
+            {[0, 1].map((row) => (
+                <div className={`row ${row === 0 ? 'left' : 'right'}`} key={row}>
+                {[...repeatedImages, ...repeatedImages].map((_, idx) => (
+                    <img
+                    key={idx}
+                    src="/example.png"
+                    alt="example"
+                    className="photo"
+                    />
+                ))}
+                </div>
+            ))}
             </div>
         </div>
 
